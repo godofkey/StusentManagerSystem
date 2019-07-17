3509379d3a53bf0fd9d92833fd7efab9f6fefa10
zhupei <2505640524@qq.com>
2019-07-17 09:48:34 +0800

    请求修改

--- sms-parent/sms-web/src/main/java/com/dreamworks/sms/student/controller/StudentLoginController.java
+++ sms-parent/sms-web/src/main/java/com/dreamworks/sms/student/controller/StudentLoginController.java
@@ -7,6 +7,7 @@ import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import com.dreamworks.sms.student.service.StudentLoginService;
 
 @RestController
 @RequestMapping("/student")
+@CrossOrigin
 public class StudentLoginController {
    
 	@Autowired
