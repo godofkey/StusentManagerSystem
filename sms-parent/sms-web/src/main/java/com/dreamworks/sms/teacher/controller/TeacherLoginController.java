4fdf1daa97b5cc807070707eedcb6cdba23ae9de
zhupei <2505640524@qq.com>
2019-08-02 08:50:50 +0800

    mysql地址修改

--- sms-parent/sms-web/src/main/java/com/dreamworks/sms/teacher/controller/TeacherLoginController.java
+++ sms-parent/sms-web/src/main/java/com/dreamworks/sms/teacher/controller/TeacherLoginController.java
@@ -9,6 +9,7 @@ import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,7 @@ import com.dreamworks.sms.teacher.service.TeacherLoginService;
 
 @RestController
 @RequestMapping("/teacher")
+@CrossOrigin
 public class TeacherLoginController {
 
     @Autowired
